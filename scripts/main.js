ec13b12557b7d5f8fe7ee9cef5c43ac1ca455599
vidleyn <olendavsellen@gmail.com>
2025-06-19 19:40:28 +0300

    add responsiveness, layout change

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ import { updateTime, date } from "./dateTime.js";
 import { changeBackground } from "./background.js";
 import { updateDisplayWeather } from "./weather.js";
 
-const buttonTest = document.querySelector(".utility__random-background");
+const buttonTest = document.querySelector(".random-background");
 buttonTest.addEventListener("click", () => changeBackground(getRandomInt(0, 23)));
 
 updateTime();
